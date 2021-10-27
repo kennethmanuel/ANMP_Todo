93a33af4eacc6e0512af2875dbd5327a1f7f2753
Kenneth Manuel <kennethlieyanto99@gmail.com>
2021-10-28 00:21:58 +0700

    Adding fragments & Layout

--- app/src/main/java/com/kennethmanuel/todoapp/view/MainActivity.kt
+++ app/src/main/java/com/kennethmanuel/todoapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kennethmanuel.todoapp
+package com.kennethmanuel.todoapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kennethmanuel.todoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
