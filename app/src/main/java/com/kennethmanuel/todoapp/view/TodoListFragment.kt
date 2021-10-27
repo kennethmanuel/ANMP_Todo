b315874d0fad80013a688fa8f57b2f2277143482
Kenneth Manuel <kennethlieyanto99@gmail.com>
2021-10-28 01:01:04 +0700

    ViewModel

--- app/src/main/java/com/kennethmanuel/todoapp/view/TodoListFragment.kt
+++ app/src/main/java/com/kennethmanuel/todoapp/view/TodoListFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.kennethmanuel.todoapp.R
+import com.kennethmanuel.todoapp.viewmodel.ListTodoViewModel
 import kotlinx.android.synthetic.main.fragment_todo_list.*
 
 class TodoListFragment : Fragment() {
+    private lateinit var viewModel:ListTodoViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,9 +25,18 @@ class TodoListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(this).get(ListTodoViewModel::class.java)
+        viewModel.refresh()
+
         fabAdd.setOnClickListener {
             val action = TodoListFragmentDirections.actionCreateTodo()
             Navigation.findNavController(it).navigate(action)
         }
+
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+
     }
 }
